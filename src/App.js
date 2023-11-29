98337bdc6a1f627f9e3939c31e9b85462cc87349
pierre <pierre@ND.fr>
2023-11-29 13:54:00 +0100

    cc

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [myState, setMyState] = useState(["tache1", "tach2", "tache3"]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        {myState.map((element) => {
+          return <div>{element}</div>;
+        })}
+        <button
+          onClick={() => {
+            // 1 FAIRE UNE HARD COPY
+            const newArr = JSON.parse(JSON.stringify(myState));
+
+            // 2 FAIRE DES OPERATIONS SUR MON TABLEAU OU MON OBJET
+            newArr.push("toto");
+
+            // 3 REMPLACER LE STATE PAR LE NOUVEAU
+            setMyState(newArr);
+          }}
         >
-          Learn React
-        </a>
+          +
+        </button>
       </header>
     </div>
   );
